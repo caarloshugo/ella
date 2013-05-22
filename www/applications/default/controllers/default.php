cf9e24514e5eb7d1143c63e37786e6be109a92e4
Carlos Hugo Gonzalez Castell <caarloshugo@gmail.com>
2013-05-22 00:46:10 -0500

    add intenerary

--- www/applications/default/controllers/default.php
+++ www/applications/default/controllers/default.php
@@ -15,23 +15,23 @@ class Default_Controller extends ZP_Controller {
 		
 		$this->users = array(
 			"jasmina-brankovic" => array("name" => "Jasmina Brankovic", 	  				   "language" => "en"),
-			"moses-hategeka"    => array("name" => "Moses Hategeka", 	  					   "language" => "en"),
-			"nilima-rai" 	    => array("name" => "Nilima Rai", 	 				 		   "language" => "en"),
+			"moses-hategeka"    => array("name" => "Moses Hategeka", 	  					   "language" => "en", "flight2" => true),
+			"nilima-rai" 	    => array("name" => "Nilima Rai", 	 				 		   "language" => "en", "flight2" => true),
 			"ghislain-koffi"    => array("name" => "Ghislain Koffi Dodji Nyaku", 	  		   "language" => "en"),
-			"buddha-narayan"    => array("name" => "Buddha Narayan Sahani Kewat", 	 		   "language" => "en"),
-			"ronald-ssenfuka"   => array("name" => "Ronald Ssenfuka", 	  					   "language" => "en"),
+			"buddha-narayan"    => array("name" => "Buddha Narayan Sahani Kewat", 	 		   "language" => "en", "flight2" => true),
+			"ronald-ssenfuka"   => array("name" => "Ronald Ssenfuka", 	  					   "language" => "en", "flight2" => true),
 			"abu-bakar"  		=> array("name" => "Abu Bakar Siddique", 	  				   "language" => "en"),
 			"margaret-onwanyi"  => array("name" => "Margaret Onwanyi Onah", 	  			   "language" => "en"),
 			"syed-naeem"  		=> array("name" => "Syed Naeem Ather Abbas", 	  			   "language" => "en"),
 			"mukhtar-paras" 	=> array("name" => "Mukhtar Paras Shah", 	 				   "language" => "en"),
-			"venessa-claudette" => array("name" => "Venessa Claudette Thigesharee Padayachee", "language" => "en"),
-			"david-oluremi"     => array("name" => "David Oluremi Tola Winjobi", 	  		   "language" => "en", "flight" => "MORALES-Diego")
+			"venessa-claudette" => array("name" => "Venessa Claudette Thigesharee Padayachee", "language" => "en")
 		); 
 	}
 	
 	public function index($user = FALSE) {
 		if($user != FALSE and isset($this->users[$user])) {
-			$vars["user"] = $this->users[$user];
+			$vars["user"]    = $this->users[$user];
+			$vars["user_id"] = $user;
 			$vars["view"] = $this->view("home", TRUE);
 			
 			$this->render("content", $vars);
--- www/applications/default/views/home.php
+++ www/applications/default/views/home.php
@@ -6,71 +6,42 @@
 
 <div id="buttons">
 	
-	<?php if($user["language"] == "es") { ?>
-		<div class="icon info-icon info-icon"></div>
-		<div class="icon trabajar-icon trabajar-icon"></div>
-		<div class="icon documentos-icon documentos-icon"></div>
-	<?php } else { ?>
-		<div class="icon info-icon info-icon"></div>
-		<div class="icon trabajar-icon trabajar-icon"></div>
-		<div class="icon documentos-icon documentos-icon"></div>
-	<?php } ?>
+	<div class="icon info-icon info-icon"></div>
+	<div class="icon trabajar-icon trabajar-icon"></div>
+	<div class="icon documentos-icon documentos-icon"></div>
+	
 	
 	<div class="section info">
 		<ul>
 			<li class="doc">
-				<?php if($user["language"] == "es") { ?>
-					<a href="<?php print get('webURL'); ?>/documents/info/Programa_Seminario.docx" title="Programa">Programa</a>
-				<?php } else { ?>
-					<a href="<?php print get('webURL'); ?>/documents/info/Seminar_Program.docx" title="Program">Program</a>
-				<?php } ?>
+				<a href="<?php print get('webURL'); ?>/documents/info/Seminar_Program.docx" title="Program">Program</a>
 			</li>
-			
-			<?php if($user["language"] == "es") { ?>
-				<li><a href="<?php print get('webURL'); ?>/documents/info/Logistica_Seminario_Presupuestos.pdf" title="Logistica Seminario Presupuestos">Log&iacute;stica Seminario Presupuestos</a></li>
-			<?php } ?>
 
 			<li>
-				<?php if($user["language"] == "es") { ?>
-					<a href="<?php print get('webURL'); ?>/documents/info/Agenda_Invitados_Internacionales.pdf" title="Agenda Invitados Internacionales">Agenda Invitados Internacionales</a>
-				<?php } else { ?>
-					<a href="<?php print get('webURL'); ?>/documents/info/Agenda_International_Participants.pdf" title="Agenda International Participants">Agenda International Participants</a>
-				<?php } ?>
+				<a href="<?php print get('webURL'); ?>/documents/info/Agenda_International_Participants.pdf" title="Agenda International Participants">Agenda International Participants</a>
 			</li>
 			
-			<?php if(isset($user["flight"])) { ?>
+			<li>
+				<a href="<?php print get('webURL'); ?>/documents/flights/itinerary-<?php echo $user_id;?>.pdf" title="Itinerary">Itinerary</a>
+			</li>
+			
+			<?php if(isset($user["flight2"])) { ?>
 				<li>
-					<?php if($user["language"] == "es") { ?>
-						<a href="<?php print get('webURL'); ?>/documents/flights/<?php echo $user["flight"];?>.pdf" title="Vuelo">Vuelo</a>
-					<?php } else { ?>
-						<a href="<?php print get('webURL'); ?>/documents/flights/<?php echo $user["flight"];?>.pdf" title="Flight">Flight</a>
-					<?php } ?>
+					<a href="<?php print get('webURL'); ?>/documents/flights/itinerary-<?php echo $user_id;?>2.pdf" title="Itinerary 2">Itinerary 2</a>
 				</li>
 			<?php } ?>
 		</ul>
 		
-		<?php if($user["language"] == "es") { ?>
-			<div class="section-icons-min">
-				<div class="min-icon trabajar-icon-min trabajar-icon-min-es"></div>
-				<div class="min-icon documentos-icon-min documentos-icon-min-es"></div>
-			</div>
-		<?php } else { ?>
-			<div class="section-icons-min">
-				<div class="min-icon trabajar-icon-min trabajar-icon-min-en"></div>
-				<div class="min-icon documentos-icon-min documentos-icon-min-en"></div>
-			</div>
-		<?php } ?>
+		<div class="section-icons-min">
+			<div class="min-icon trabajar-icon-min trabajar-icon-min-en"></div>
+			<div class="min-icon documentos-icon-min documentos-icon-min-en"></div>
+		</div>
 	</div>
-	
 	
 	<div class="section trabajar">
 		<ul>
 			<li>
-				<?php if($user["language"] == "es") { ?>
-					<a href="<?php print get('webURL'); ?>/documents/work/Caso_de_Estudio_ACIJ.pdf" title="Caso de Estudio ACIJ">Caso de Estudio ACIJ</a>
-				<?php } else { ?>
-					<a href="<?php print get('webURL'); ?>/documents/work/ACIJ-summary.pdf" title="ACIJ Summary">ACIJ Summary</a>
-				<?php } ?>
+				<a href="<?php print get('webURL'); ?>/documents/work/ACIJ-summary.pdf" title="ACIJ Summary">ACIJ Summary</a>
 			</li>
 			
 			<li>
@@ -80,43 +51,17 @@
 			<li class="doc">
 				<a href="<?php print get('webURL'); ?>/documents/work/Amasango_write_up.docx" title="Amasango write up">Amasango write up</a>
 			</li>
-			
-			
 		</ul>
 		
-		<?php if($user["language"] == "es") { ?>
-			<div class="section-icons-min">
-				<div class="min-icon info-icon-min info-icon-min-es"></div>
-				<div class="min-icon documentos-icon-min documentos-icon-min-es"></div>
-			</div>
-		<?php } else { ?>
-			<div class="section-icons-min">
-				<div class="min-icon info-icon-min info-icon-min-en"></div>
-				<div class="min-icon documentos-icon-min documentos-icon-min-en"></div>
-			</div>
-		<?php } ?>
+		
+		<div class="section-icons-min">
+			<div class="min-icon info-icon-min info-icon-min-en"></div>
+			<div class="min-icon documentos-icon-min documentos-icon-min-en"></div>
+		</div>
 	</div>
 	
 	<div class="section documentos">
-		<ul>
-			<li class="link">
-				<a target="_blank" href="http://www.inesc.org.br/news/2012/april/budget-monitoring-from-a-human-rights-perspective" title="Budget Monitoring - From a Human Rights perspective (INESC)">
-					Budget Monitoring - From a Human Rights perspective (INESC)
-				</a>
-			</li>
-			
-			<li class="link">
-				<a target="_blank" href="http://www.humanrightsbudgetwork.org/" title="IHRIP Institute of International Education Human rights budget work">
-					IHRIP Institute of International Education Human rights budget work
-				</a>
-			</li>
-			
-			<li>
-				<a href="<?php print get('webURL'); ?>/documents/documents/Guia-de-Analisis-Presupuestario-ACIJ.pdf" title="Guía de Análisis Presupuestario ACIJ">
-					Gu&iacute;a de An&aacute;lisis Presupuestario ACIJ
-				</a>
-			</li>
-			
+		<ul>			
 			<li>
 				<a href="<?php print get('webURL'); ?>/documents/documents/Politicas-publicas-y-presupuestos-con-perspectiva-de-derechos-humanos-OACNUDH-et-al.pdf" title="Políticas públicas y presupuestos con perspectiva de derechos humanos OACNUDH">
 					Pol&iacute;ticas p&uacute;blicas y presupuestos con perspectiva de derechos humanos OACNUDH
@@ -154,16 +99,9 @@
 			</li>
 		</ul>
 		
-		<?php if($user["language"] == "es") { ?>
-			<div class="section-icons-min">
-				<div class="min-icon info-icon-min info-icon-min-es"></div>
-				<div class="min-icon trabajar-icon-min trabajar-icon-min-es"></div>
-			</div>
-		<?php } else { ?>
-			<div class="section-icons-min">
-				<div class="min-icon info-icon-min info-icon-min-en"></div>
-				<div class="min-icon trabajar-icon-min trabajar-icon-min-en"></div>
-			</div>
-		<?php } ?>
+		<div class="section-icons-min">
+			<div class="min-icon info-icon-min info-icon-min-en"></div>
+			<div class="min-icon trabajar-icon-min trabajar-icon-min-en"></div>
+		</div>
 	</div>
 </div>

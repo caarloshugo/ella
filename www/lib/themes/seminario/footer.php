a31a453cf107cf4ac4a2d4339f1f76bbf8f9bf31
Carlos Hugo Gonzalez Castell (Fundar) <carlos@fundar.org.mx>
2013-05-23 16:05:14 -0500

    links - footer

--- www/lib/themes/seminario/footer.php
+++ www/lib/themes/seminario/footer.php
@@ -1,7 +1,7 @@
 			<div class="clear"></div>
 			
 			<div id="footer">
-				<a href="http://fundar.org.mx" title="Practial action consulting">
+				<a href="http://practicalaction.org/consulting-3" title="Practial action consulting">
 					<img src="<?php print $this->themePath; ?>/css/images/logo-practical-action.png" alt="Practial action consulting" />
 				</a>
 				
@@ -9,10 +9,10 @@
 					<img src="<?php print $this->themePath; ?>/css/images/fundar.jpg" alt="Fundar.org.mx" />
 				</a>
 				
-				<a href="http://fundar.org.mx" title="UK aid">
+				<a href="https://www.gov.uk/government/organisations/department-for-international-development" title="UK aid">
 					<img src="<?php print $this->themePath; ?>/css/images/logo-ukaid.png" alt="UK aid" />
 				</a>
-				<a href="http://fundar.org.mx/ella" title="ELLA">
+				<a href="http://ella.practicalaction.org" title="ELLA">
 					<img src="<?php print $this->themePath; ?>/css/images/logo-ella.png" alt="ELLA" />
 				</a>
 			</div>
